e16322e8c4153eb1034ea0ad8fe26a5335f6882b
赵福成 <zhaofucheng1129@gmail.com>
2019-04-22 23:25:27 +0800

    Init

---
+++ GreatApp/GreatApp-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#include <ifaddrs.h>
+
+#if __has_include(<webp/decode.h>) && __has_include(<webp/encode.h>) && __has_include(<webp/demux.h>)  && __has_include(<webp/mux.h>)
+#import <webp/decode.h>
+#import <webp/encode.h>
+#import <webp/demux.h>
+#import <webp/mux.h>
+#elif __has_include("webp/decode.h") && __has_include("webp/encode.h") && __has_include("webp/demux.h")  && __has_include("webp/mux.h")
+#import "webp/decode.h"
+#import "webp/encode.h"
+#import "webp/demux.h"
+#import "webp/mux.h"
+#endif
---
+++ GreatApp/Image/WebImageManager.swift
@@ -0,0 +1,53 @@
+//
+//  WebImageManager.swift
+//  GreatApp
+//
+//  Created by 赵福成 on 2019/4/22.
+//  Copyright © 2019 zhaofucheng. All rights reserved.
+//
+
+import UIKit
+
+public typealias DownloadProgressBlock = ((_ receivedSize: Int64, _ totalSize: Int64) -> Void)
+
+public class WebImageManager {
+    public static let shared = WebImageManager()
+    
+    public var defaultOptions = WebImageOptionsInfo.empty
+    
+    public var downloader: WebImageDownloader
+    
+    private let processingQueue: DispatchQueue
+    
+    private convenience init() {
+        self.init(downloader: .default)
+    }
+    
+    init(downloader: WebImageDownloader) {
+        self.downloader = downloader
+        processingQueue = DispatchQueue(label: "com.zhaofucheng.GreatApp.WebImageManager.processQueue.\(String.UUID())")
+    }
+    
+    func retrieveImage(with url: URL, options: WebImageParsedOptionsInfo,
+                       progressBlock: DownloadProgressBlock? = nil,
+                       completionHandler: ((Result<Image, WebImageError>) -> Void)?) -> DownloadTask? {
+        if options.forceRefresh {
+            return loadAndCacheImage(with: url, options: options, completionHandler: completionHandler)
+        } else {
+            return loadAndCacheImage(with: url, options: options, completionHandler: completionHandler)
+        }
+    }
+    
+    func loadAndCacheImage(with url: URL, options: WebImageParsedOptionsInfo,
+                           progressBlock: DownloadProgressBlock? = nil,
+                           completionHandler: ((Result<Image, WebImageError>) -> Void)?) -> DownloadTask? {
+        
+        func cacheImage(_ result: Result<Image, WebImageError>) {
+            completionHandler?(result)
+        }
+        
+        return downloader.downloadImage(with: url, options: options, progressBlock: progressBlock, completionHandler: cacheImage)
+    }
+    
+}
+
